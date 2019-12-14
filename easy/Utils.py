e9c0f4c2aeb61d0483f7bbe24c83aab8d1a4db99
npado <nicola.padovano@mediaset.it>
2019-12-15 00:47:15 +0100

    * added other useful methods

--- easy/Utils.py
+++ easy/Utils.py
@@ -3,6 +3,7 @@ import json
 import hashlib
 from botocore.vendored import requests
 from botocore.vendored.requests.adapters import HTTPAdapter
+# from requests.adapters import HTTPAdapter
 from botocore.vendored.requests.packages.urllib3.util.retry import Retry
 
 
